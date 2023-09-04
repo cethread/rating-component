0fe9a01610c3dc16cd0265804ce144c3f8ce002c
cethread <cethread@aggies.ncat.edu>
2023-09-04 11:59:23 -0400

    inital commit

---
+++ thank-you.js
@@ -0,0 +1,27 @@
+//DONT NEED THIS PAGE
+
+
+//NEED RATING NUMBER FROM OTHER PAGE
+    // SHOWING ERROR Uncaught TypeError: document.querySelector(...) is null
+        //SHOWING BECAUSE THE RATING ELEMENTS ARENT ON THE THANK YOU PAGE
+// const ratingBtn = document.querySelector(".rating-btn");
+// const ratingNumber = document.querySelector(".rating-btn").innerHTML;
+
+
+// const thankyouBtn = document.querySelector(".thank-you-card__btn");
+// make switch statement based off of selected rating
+// create thank you page
+    // In thank you page, switch rating number based on user rating
+
+// function ratingSelection () {
+//     switch (ratingBtn) { 
+//         case [0]: 
+//             thankyouBtn.innerHTML = `"You selected ${ratingNumber} out of 5"`;
+//             window.location.assign("thank-you.html");
+//             break
+//     }
+// };
+
+// add event listener to change the page view for when a rating is clicked
+// ratingBtn.forEach((btn) => btn.addEventListener("click", ratingSelection));
+// ratingBtn.addEventListener("click", ratingSelection);
